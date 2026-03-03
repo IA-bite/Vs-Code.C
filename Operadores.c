a025802f4d745428437fbad37ebedf8bfcf391e3
Clyverton Carlos <Clyverton10empresario@gmail.com>
2026-03-03 20:31:27 +0000

    Operações 2

---
+++ Operadores.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+
+/*
+Atribuição Simples (=)
+Atribição com soma (+=)
+Atribuição com subtação (-+)
+Atribuição com Multiplicação (*=)
+Atribuição com Divisão (/=)
+*/
+
+int numero1 = 10, numero2, resultado;
+
+resultado =10;
+printf ("resultado: %d\n", resultado);
+
+// resultado = resultado + 20//
+
+resultado += 20;
+printf ("resultado: %d\n", resultado);
+
+//resultado = resultado - numero1;//
+
+resultado -= numero1;
+printf ("resultado: %d\n", resultado);
+
+// resultado = resultado*5//
+
+resultado *= 5;
+printf ("resultado: %d\n", resultado);
+
+//resultado = resultado/2//
+
+resultado /= 2;
+printf ("resultado: %d\n", resultado);
+
+
+}
